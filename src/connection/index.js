9d867ce0ef627a3e2fea382de7dc36b6304f39b2
ihsanazmi <ihsanazmiii@gmail.com>
2020-01-24 09:58:59 +0700

    deploy to heroku

--- src/connection/index.js
+++ src/connection/index.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    user: "devuser",
+    // user: "devuser",
+    // password: "Ihsanazmi26",
+    // host: "localhost",
+    // database: "ecommerce",
+    // port: 3306
+    user: "kumisproject",
     password: "Ihsanazmi26",
-    host: "localhost",
-    database: "ecommerce",
+    host: "db4free.net",
+    database: "ecommerce_kumis",
     port: 3306
 })
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const transactionRouter = require('./routers/transactionRouter')
 const reviewRouter = require('./routers/reviewRouter')
 
 const app = express()
-const port = 2018
+const port = process.env.PORT || 2019
+// const port = 2018
 
 app.use(cors())
 app.use(express.json())
@@ -23,6 +24,10 @@ app.use(kategoriRouter)
 app.use(cartRouter)
 app.use(transactionRouter)
 app.use(reviewRouter)
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Running at ${port}</h1>`)
+})
 
 app.listen(port, ()=>{
     console.log(`running at port ${port}`)
